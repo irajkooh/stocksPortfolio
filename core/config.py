9c69c1954f79dede5d7747f7ee3672815d9335f1
irajkooh <irajkooh@users.noreply.github.com>
2026-08-25 08:23:52 -0500

    deploy: 2026-08-25 08:23

--- core/config.py
+++ core/config.py
@@ -13,7 +13,8 @@ DATA_DIR.mkdir(parents=True, exist_ok=True)
 
 # LLM
 GROQ_API_KEY  = os.environ.get("GROQ_API_KEY", "")
-GROQ_MODEL    = os.environ.get("GROQ_MODEL", "llama-3.1-8b-instant")
+#GROQ_MODEL    = os.environ.get("GROQ_MODEL", "llama-3.1-8b-instant")
+GROQ_MODEL    = os.environ.get("GROQ_MODEL", "openai/gpt-oss-20b")
 OLLAMA_URL    = os.environ.get("OLLAMA_BASE_URL", "http://localhost:11434")
 OLLAMA_MODEL  = os.environ.get("OLLAMA_MODEL", "llama3")
 
